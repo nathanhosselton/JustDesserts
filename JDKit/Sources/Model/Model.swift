0666814a54f69f1f49afe4b8c8142ac7685d8570
Nathan Hosselton <nathanhosselton@gmail.com>
2024-05-07 12:01:31 -0500

    feat: create package JDKit
    - will contain Model and Services modules
    - rename dir JustDesserts -> App

---
+++ JDKit/Sources/Model/Model.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+/// The container object for model data utilized throughout the app.
+public final class Model: ObservableObject {
+
+}
---
+++ JDKit/Sources/Model/Services.swift
@@ -0,0 +1,4 @@
+/// An interface for a type which enables network operations on the Model's behalf.
+public protocol NetworkService {
+
+}
