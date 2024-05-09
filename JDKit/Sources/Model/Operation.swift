c570a7981ab8388643166b8a795c9f336bff38d0
Nathan Hosselton <nathanhosselton@gmail.com>
2024-05-09 14:31:08 -0500

    feat: create protocol Operation

---
+++ JDKit/Sources/Model/Operation.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+/// An interface for an object representing a specific operation executed by one of our `Services`.
+protocol Operation {
+  /// The type into which this operation's response data decodes.
+  associatedtype ResponseType: Decodable
+
+  /// The fully formed URLRequest that may be used to execute this operation via the `NetworkService`.
+  var urlRequest: URLRequest { get }
+  /// Encapsulates the decoding logic for converting this operation's response data into a public Model type.
+  func decode(data: Data, using decoder: JSONDecoder) throws -> ResponseType
+}
