68a5679c52bc9fc2db5eb50d21ede87c7bdbd08b
Nathan Hosselton <nathanhosselton@gmail.com>
2024-05-09 18:20:52 -0500

    tests: GetDessertDetail/decode(data:using:)
    - test that decoding valid data succeeds
    - tests that decoding empty results throws the expected error

--- JDKit/Tests/ModelTests/ModelTests.swift
+++ JDKit/Tests/ModelTests/ModelTests.swift
@@ -24,7 +24,7 @@ final class ModelTests: XCTestCase {
         isFetchingDessertsValues.append($0)
       }
 
-    // When reloadDesserts() is executed
+    // When refreshDesserts() is executed
     try await model.refreshDesserts()
     cancellable.cancel()
 
@@ -33,5 +33,54 @@ final class ModelTests: XCTestCase {
       isFetchingDessertsValues,
       [false, true, false]
     )
+  }
+
+  func test_Operation_GetDessertDetail_decode() throws {
+    // Given a newly initialized GetDessertDetail object and a fixture of a valid response
+    let operation = GetDessertDetail(dessertId: "")
+    let fixture = #"""
+    {
+      "meals": [
+        {
+          "idMeal": "53049",
+          "strMeal": "Apam balik",
+          "strInstructions": "Mix milk, oil and egg together.\r\n\r\nSpread.",
+          "strMealThumb": "https://www.themealdb.com/images/media/meals/adxcbq1619787919.jpg",
+          "strIngredient1": "Milk",
+          "strMeasure1": "200ml",
+        }
+      ]
+    }
+    """#
+      .data(using: .utf8)!
+
+    // When decoding the response data through the operation
+    let decoded = try? operation.decode(data: fixture, using: JSONDecoder())
+
+    // Then the decoded object is non-nil (decoding succeeds)
+    XCTAssert(decoded != nil)
+  }
+
+  func test_Operation_GetDessertDetail_decodingEmptyMealsThrows_permanentResponseFailure() throws {
+    // Given a newly initialized GetDessertDetail object and a fixture of a response containing no details
+    let operation = GetDessertDetail(dessertId: "")
+    let fixture = #"""
+    {
+      "meals": [
+      ]
+    }
+    """#
+      .data(using: .utf8)!
+    var intentionalError: Error? = nil
+
+    // When decoding the response data through the operation
+    do {
+      _ = try operation.decode(data: fixture, using: JSONDecoder())
+    } catch {
+      intentionalError = error
+    }
+
+    // Then the caught error is ModelError.permanentResponseFailure
+    XCTAssert(intentionalError as? ModelError == .permanentResponseFailure)
   }
 }
