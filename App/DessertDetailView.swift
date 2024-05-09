e884d7a76b8bbc63c951171a2eaa0cdec96d853d
Nathan Hosselton <nathanhosselton@gmail.com>
2024-05-09 17:24:41 -0500

    refactor: DessertDetailView body into separate builders
    - extract the contents of the ingredients and steps sections into
    separate builder functions
    - extract the conditional, embedded error content into a separate
    builder function (mostly to hide the verbose comments)

--- App/DessertDetailView.swift
+++ App/DessertDetailView.swift
@@ -13,7 +13,8 @@ struct DessertDetailView: View {
 
   var body: some View {
     ScrollView {
-      VStack(alignment: .leading, spacing: 0) {
+      VStack(spacing: 0) {
+        // - Header image
         AsyncImage(url: dessert.thumbnail) { image in
           image.resizable()
             .scaledToFill()
@@ -24,71 +25,21 @@ struct DessertDetailView: View {
         .clipped()
 
         VStack(alignment: .leading) {
+          // - Dessert name
           Text(dessert.name)
             .font(.largeTitle.weight(.medium))
             .padding(.bottom, 20)
 
           Group {
             if let details = details {
-              // Display the fetched dessert details
-              VStack(alignment: .leading, spacing: 6) {
-                Text("Ingredients")
-                  .font(.headline)
-                  .padding(.bottom, 6)
+              // - Ingredients and Steps
+              IngredientsSection(displaying: details.ingredients)
+                .padding(.bottom, 20)
 
-                ForEach(details.ingredients) { item in
-                  HStack {
-                    Text(item.name.capitalized)
-                    Spacer()
-                    Text(item.amount)
-                  }
-                }
-              }
-              .padding(.bottom, 20)
-
-              VStack(alignment: .leading, spacing: 12) {
-                Text("Steps")
-                  .font(.headline)
-
-                ForEach(details.steps, id: \.self) { step in
-                  Text(step)
-                    .fixedSize(horizontal: false, vertical: true)
-                }
-              }
+              StepsSection(displaying: details.steps)
             } else if let error = lastError {
-              // An error occurred while fetching the dessert details
-              switch error {
-              case ModelError.permanentResponseFailure:
-                // We received the details but they were not well formed.
-                //
-                // - Note: This state is essentially our handling of the case where a DessertResult
-                // from the /filter API call is well-formed, but the corresponding DessertDetail
-                // from the /lookup API call is malformed. Based on the current decoding implementation
-                // within DessertDetail, this can only happen if every ingredient or measurement 
-                // is null, or if the instructions are null. Neither of these is currently the case
-                // in the API for any entries returned under the Dessert category.
-                //
-                // The alternative to this was to perform all /lookup requests immediately upon app
-                // launch and cache them in advance. This would have the advantage of both knowing
-                // in advance if a DessertDetail will fail to decode, allowing us to throw away the
-                // corresponding DessertResult before showing it to the user, as well as skipping
-                // a loading state when the user taps on a DessertResult entry in DessertListView.
-                //
-                // In reality, that lookup should rarely take longer than the transition time to
-                // the DessertDetailView. And performing that many API requests simultaneously or
-                // even in sequence has the potential to overload the service or result in a rate
-                // limit. Using a view transition to mask the transit time of an additional data
-                // request is also a more broadly used strategy than pre-fetching, and so I chose
-                // to go this route rather than the alternative.
-                Text("Details for this dessert are\nnot currently available.")
-              default:
-                // Notify the user and allow them to retry.
-                VStack(alignment: .center) {
-                  Text("Failed while retrieving this dessert.")
-                  Button("Retry", action: retrieveDetails)
-                    .buttonStyle(BorderedButtonStyle())
-                }
-              }
+              // - Embedded error display
+              ErrorContent(for: error)
             } else {
               // Details are still in-flight.
               ProgressView()
@@ -99,6 +50,74 @@ struct DessertDetailView: View {
       }
     }
     .onAppear(perform: retrieveDetails)
+  }
+
+  /// Builds the section displaying the dessert's recipe ingredients and corresponding measurements from
+  /// the fetched `DessertDetail`.
+  private func IngredientsSection(displaying ingredients: [DessertDetail.Ingredient]) -> some View {
+    VStack(alignment: .leading, spacing: 6) {
+      Text("Ingredients")
+        .font(.headline)
+        .padding(.bottom, 6)
+
+      ForEach(ingredients) { item in
+        HStack {
+          Text(item.name.capitalized)
+          Spacer()
+          Text(item.amount)
+        }
+      }
+    }
+  }
+
+  /// Builds the section displaying the dessert's recipe streps from the fetched `DessertDetail`.
+  private func StepsSection(displaying steps: [String]) -> some View {
+    VStack(alignment: .leading, spacing: 12) {
+      Text("Steps")
+        .font(.headline)
+
+      ForEach(steps, id: \.self) { step in
+        Text(step)
+          .fixedSize(horizontal: false, vertical: true)
+      }
+    }
+  }
+
+  /// Builds conditional content for display when an error occurs while fetching the dessert details.
+  @ViewBuilder
+  private func ErrorContent(for error: Error) -> some View {
+    switch error {
+    case ModelError.permanentResponseFailure:
+      // We received the details but they were not well formed.
+      //
+      // - Note: This state is essentially our handling of the case where a DessertResult
+      // from the /filter API call is well-formed, but the corresponding DessertDetail
+      // from the /lookup API call is malformed. Based on the current decoding implementation
+      // within DessertDetail, this can only happen if every ingredient or measurement
+      // is null, or if the instructions are null. Neither of these is currently the case
+      // in the API for any entries returned under the Dessert category.
+      //
+      // The alternative to this was to perform all /lookup requests immediately upon app
+      // launch and cache them in advance. This would have the advantage of both knowing
+      // in advance if a DessertDetail will fail to decode, allowing us to throw away the
+      // corresponding DessertResult before showing it to the user, as well as skipping
+      // a loading state when the user taps on a DessertResult entry in DessertListView.
+      //
+      // In reality, that lookup should rarely take longer than the transition time to
+      // the DessertDetailView. And performing that many API requests simultaneously or
+      // even in sequence has the potential to overload the service or result in a rate
+      // limit. Using a view transition to mask the transit time of an additional data
+      // request is also a more broadly used strategy than pre-fetching, and so I chose
+      // to go this route rather than the alternative.
+      Text("Details for this dessert are\nnot currently available.")
+    default:
+      // Notify the user and allow them to retry.
+      VStack(alignment: .center) {
+        Text("Failed while retrieving this dessert.")
+        Button("Retry", action: retrieveDetails)
+          .buttonStyle(BorderedButtonStyle())
+      }
+    }
   }
 
   /// Asks the `model` to get the details for our `dessert` then updates our `details` with the results.
